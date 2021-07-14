556c88bd2c243bbba4f3c62b7c2a207045e9dde5
robkpaul <rob.k.paul@gmail.com>
2021-07-13 18:35:26 -0700

    Basic bot outline

--- bot.py
+++ bot.py
@@ -1,5 +1,13 @@
 from dotenv import load_dotenv
-import discord, os
+import discord, logging, os
 
 load_dotenv()
 bot_token = os.getenv('DISCORD_BOT_TOKEN')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('Logged in as {}'.format(client.user))
+
+client.run(bot_token)
