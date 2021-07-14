6d6c49831a7d64c53ff76387d8f5e27eef5dc43e
robkpaul <rob.k.paul@gmail.com>
2021-07-13 20:11:28 -0700

    Added sheets file

--- bot.py
+++ bot.py
@@ -1,8 +1,16 @@
 from dotenv import load_dotenv
-import discord, logging, os
+from oauth2client.service_account import ServiceAccountCredentials
+import discord, logging, os, gspread
 
+logging.basicConfig(level=logging.INFO)
 load_dotenv()
 bot_token = os.getenv('DISCORD_BOT_TOKEN')
+
+gspread_creds = ServiceAccountCredentials.from_json_keyfile_name('google_sheets_key.json', scopes = ['https://www.googleapis.com/auth/spreadsheets','https://www.googleapis.com/auth/drive'])
+gspread_client = gspread.authorize(gspread_creds)
+gamelog = gspread_client.open('Warden Campaign Tracker').worksheet('Session Log')
+
+gamelog.update_cell(100, 1, "Hello World!")
 
 client = discord.Client()
 
