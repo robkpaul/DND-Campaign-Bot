679d8284400afcd298a21fd2c6bd82489ecd368e
robkpaul <rob.k.paul@gmail.com>
2021-07-16 16:06:49 -0700

    quest command partial implementation

--- bot.py
+++ bot.py
@@ -1,38 +1,44 @@
 from dotenv import load_dotenv
 from oauth2client.service_account import ServiceAccountCredentials
-import discord, logging, os, gspread, re
+import discord, logging, os, re, datetime
 
 logging.basicConfig(level=logging.INFO)
 load_dotenv()
 bot_token = os.getenv('DISCORD_BOT_TOKEN')
 
-gspread_creds = ServiceAccountCredentials.from_json_keyfile_name('google_sheets_key.json', scopes = ['https://www.googleapis.com/auth/spreadsheets','https://www.googleapis.com/auth/drive'])
-gspread_client = gspread.authorize(gspread_creds)
-gamelog = gspread_client.open(input("Spreadsheet for session log: ")).worksheet('Session Log')
+botClient = discord.Client()
 
-client = discord.Client()
+async def quest_command(message, args):
+    quest = {}
+    quest['title'] = args[1]
+    dateOf = datetime.date(datetime.datetime.now().year, int(args[3]), int(args[4]))
+    timeOf = datetime.time(int(args[6]), int(args[7]))
+    quest['time'] = timeOf
+    if dateOf < datetime.datetime.now().date():
+        dateOf = datetime.date(datetime.datetime.now().year+1, dateOf.month, dateOf.day)
+    quest['date'] = dateOf
+    quest['creator'] = message.author
+    await message.channel.send(quest)
 
-async def quest_command(message):
-    msg = message.content[message.content.find('"')+1:]
-
-#reads commands
+#reads commands in and directs to the correct command
 async def cmd_reader(message):
     msg = message.content
-    if msg.startswith('$quest'):
-        if('"' in msg and '"' in msg[msg.find('"')+1:] and '/' in msg and ':' in msg):
-            #quest_command(message)
-            print("quest message registered")
+    if msg.startswith('$quest '):
+        questRegex = re.compile(r'("(.{1,})") (([0-1]\d)\/(\d{2})) (([0-1]\d)\:(\d{2}))')
+        questArgs = re.match(questRegex, msg[7:])
+        if questArgs:
+            await quest_command(message, questArgs.groups())
         else:
             await message.channel.send('Incorrect format for the $quest command')
 
 
-@client.event
+@botClient.event
 async def on_ready():
-    print('Logged in as {}'.format(client.user))
+    print('Logged in as {}'.format(botClient.user))
 
-@client.event
+@botClient.event
 async def on_message(message):
-    if message.author != client.user and message.content.startswith('$'):
-        cmd_reader(message)
+    if message.author != botClient.user and message.content.startswith('$'):
+        await cmd_reader(message)
 
-client.run(bot_token)
+botClient.run(bot_token)
